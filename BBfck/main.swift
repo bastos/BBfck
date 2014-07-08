8a0dcf3b4986a8e8f1dc4e5520125ddf88462d59
Tiago Bastos <contact@tiagobastos.me>
2014-07-08 12:16:11 -0300

    Initial Commit

---
+++ BBfck/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BBfck
+//
+//  Created by Tiago Bastos on 7/8/14.
+//  Copyright (c) 2014 Tiago Bastos. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
