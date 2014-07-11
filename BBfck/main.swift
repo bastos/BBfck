8336fbabd841187ea5d162bec294221cadaa20ba
Tiago Bastos <contact@tiagobastos.me>
2014-07-11 12:53:30 -0300

    It works.

--- BBfck/main.swift
+++ BBfck/main.swift
@@ -8,5 +8,90 @@
 
 import Foundation
 
-println("Hello, World!")
+let dataStackSize = 300
 
+struct Program {
+    let instructions: [Instructions]
+    var data: [Int]
+    var ip: Int
+    var dp: Int
+}
+
+enum Instructions : Character {
+    case Increase = "+"
+    case Decrease = "-"
+    case StartLoop = "["
+    case EndLoop = "]"
+    case ReadChar = ","
+    case PrintChar = "."
+    case IncreasePointer = ">"
+    case DecreasePointer = "<"
+}
+
+func readCode(program: String) -> [Instructions] {
+    var stack = [Instructions]()
+
+    for char in program {
+        if char == "!" {
+            return stack
+        } else {
+            stack.append(Instructions.fromRaw(char)!)
+        }
+    }
+    
+    return stack
+}
+
+func executeCode(code: String) {
+    var data = Array(count: dataStackSize, repeatedValue: 0)
+    let instructions = readCode(code)
+    var program = Program(instructions: instructions, data: data, ip: 0, dp: 0)
+    
+    while program.ip < program.instructions.count {
+        let instruction = program.instructions[program.ip]
+        switch (instruction) {
+            case .IncreasePointer:
+                program.dp += 1
+            case .DecreasePointer:
+                program.dp -= 1
+            case .Increase:
+                program.data[program.dp] += 1
+            case .Decrease:
+                program.data[program.dp] -= 1
+            case .PrintChar:
+                print(Character(UnicodeScalar(program.data[program.dp])))
+            case .StartLoop:
+                var depth = 0
+                if program.data[program.dp] == 0 {
+                    program.ip += 1
+                    while depth > 0 || program.instructions[program.ip] != .EndLoop {
+                        if program.instructions[program.ip] == .StartLoop {
+                            depth += 1
+                        } else if program.instructions[program.ip] == .EndLoop {
+                            depth -= 1
+                        }
+                        program.ip += 1
+                    }
+                }
+            case .EndLoop:
+                var depth = 0
+                program.ip -= 1
+                while depth > 0 || program.instructions[program.ip] != .StartLoop {
+                    if program.instructions[program.ip] == .EndLoop {
+                        depth += 1
+                    } else if program.instructions[program.ip] == .StartLoop {
+                        depth -= 1
+                    }
+                    program.ip -= 1
+                }
+                program.ip -= 1
+            default:
+                println("ERROR \(program.instructions[program.ip].toRaw()) is not a implemented instruction")
+                exit(EXIT_FAILURE)
+        }
+        
+        program.ip += 1
+    }
+}
+
+executeCode("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.")
---
+++ README.md
@@ -0,0 +1,21 @@
+Brainfuck
+=========
+
+A Swift brainfuck interpreter.
+
+More about Brainfuck [here](http://pt.wikipedia.org/wiki/Brainfuck).
+
+
+	           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+	                   Version 2, December 2004
+
+	Copyright (C) 2014 Tiago Bastos <code@tiagobastos.me>
+
+	Everyone is permitted to copy and distribute verbatim or modified
+	copies of this license document, and changing it is allowed as long
+	as the name is changed.
+
+	           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+	  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+	 0. You just DO WHAT THE FUCK YOU WANT TO.
